c2f734efb54da971263d404a94a3311be415e831
Julio Fuentes <juliojavif@gmail.com>
2021-05-02 23:45:12 -0500

    Filtro funcionando

--- app/src/main/java/com/example/equiposdefutbol/FiltroActivity.java
+++ app/src/main/java/com/example/equiposdefutbol/FiltroActivity.java
@@ -10,6 +10,7 @@ import android.widget.SearchView;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -26,6 +27,7 @@ public class FiltroActivity extends AppCompatActivity {
         filtrar = findViewById(R.id.buscar);
         lista = findViewById(R.id.lvLista);
 
+        ec = new EquipoController(this);
         Cursor c = ec.allEquipo2();
         ArrayList<String> listado = new ArrayList<String>();
         while(c.moveToNext()){
@@ -40,6 +42,21 @@ public class FiltroActivity extends AppCompatActivity {
             @Override
             public boolean onQueryTextSubmit(String query) {
 
+                ArrayList<String> nuevaLista;
+                nuevaLista = coincidencias(ec.allEquipo2(), query);
+                if (nuevaLista.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "No hay coincidencias", Toast.LENGTH_LONG).show();
+                }else {
+                    ArrayAdapter adapter2 = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, nuevaLista);
+                    lista.setAdapter(adapter2);
+                }
+
+                /*if (listado.contains(query)){
+                    //adapter.getFilter().filter(query);
+                    Toast.makeText(FiltroActivity.this, ""+query, Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(FiltroActivity.this, "No hay coincidencias para: "+query, Toast.LENGTH_LONG).show();
+                }*/
                 return false;
             }
 
@@ -48,5 +65,17 @@ public class FiltroActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    public static ArrayList<String> coincidencias(Cursor c, String key){
+        ArrayList<String> listado = new ArrayList<String>();
+        while(c.moveToNext()){
+            if (c.getString(0).equalsIgnoreCase(key) || c.getString(4).equalsIgnoreCase(key)){
+                String equipo = c.getString(0)+" | "+c.getString(1)+" | "
+                        +c.getString(2)+" | "+c.getString(3)+" | "+c.getString(4);
+                listado.add(equipo);
+            }
+        }
+        return listado;
     }
 }
